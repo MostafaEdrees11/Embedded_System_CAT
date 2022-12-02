80683d0d9ac94bf3285b9ec52f8f14c0a8b97da8
Mostafa Edrees <mostafaedrees018@gmail.com>
2022-12-02 13:43:39 +0200

    updata the solution of problem 11

--- C Programming/Conditions & Loops/P11/11.c
+++ C Programming/Conditions & Loops/P11/11.c
@@ -7,45 +7,30 @@ Write a c code that draws a pyramid of stars with height entered by the user.
 
 int main()
 {
-	int height , n;
+	int height;
 	
-	/*
-	take the height of pyramid from user
-	*/
 	printf("Enter the height : ");
 	scanf("%d",&height);
 	
-	n = height - 1;
+	int i , j;
 	
-	/*
-	fisrt for loop is used to print num of lines equal height
-	*/	
-	for(int i = 0; i < height; i++)
+	
+	for(i = 1; i <= height; i++)
 	{
-		/*
-		second for loop is used to shift # by (h-n) time using spaces
-		*/
-		for(int j = 0; j < n; j++)
+		for(j = 1; j <= 2*height-1; j++)
 		{
-			printf(" ");
-		}
-		/*
-		third for loop is used to print num of # equal (h-n) time
-		*/
-		for(int i = 0; i < (height-n); i++)
-		{
-			printf("#");
-		}
-		/*
-		fourth for loop is used to complete the pyramid by # 
-		*/
-		for(int i = 0; i < (height-n-1); i++)
-		{
-			printf("#");
+			if(j >= height - (i-1) && j <= height + (i-1))
+			{
+				printf("#");
+			}
+			else
+			{
+				printf(" ");				
+			}
 		}
 		printf("\n");
-		n--;
 	}
+	
 	
 	
 	return 0;
